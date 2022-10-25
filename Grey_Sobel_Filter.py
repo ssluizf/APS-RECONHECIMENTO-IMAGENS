8a160ee72af89f02eb8717a06000b225d88e300e
VictoriusHorse <74615412+VictoriusHorse@users.noreply.github.com>
2022-10-24 22:54:19 -0300

    Update Grey_Sobel_Filter.py

--- Grey_Sobel_Filter.py
+++ Grey_Sobel_Filter.py
@@ -29,12 +29,12 @@ filtro_horizontal = [[ -1, 0, 1],
                      [ -2, 0, 2],
                      [ -1, 0, 1]]
 
-n,m,d = img.shape
+x,y,z = img.shape
 
 bordas_verticais_horizontais = np.zeros_like(img)
 
-for linha in range (3, n-2):
-    for coluna in range(3, m-2):
+for linha in range (3, x-2):
+    for coluna in range(3, y-2):
         pixels_locais = img[linha-1:linha+2, coluna-1:coluna+2, 0]
 
         pixels_transformados_verticais = filtro_veritical*pixels_locais
